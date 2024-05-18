25d33b05e11ec8a5295463ea6db1a1b7aea0a48a
Rohit Pandey <croods660@gmail.com>
2024-05-18 14:07:04 +0530

    some changes

---
+++ letter.py
@@ -0,0 +1,11 @@
+
+print("18 May 2024")
+
+print("I am feeling great!")
+
+print("After learning Code, i will make my projects, and i will be a great programmer")
+
+print("Hey, Myself, I think if you are reading this, then maybe you became a great programmer, and you are working on your projects and you are happy with your life, and you are living your life to the fullest, and you are enjoying!")
+
+print("My favorite emoji is: 🥲")
+
