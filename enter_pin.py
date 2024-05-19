98d7387f9e75bc9088722a3f914c4465a38b55e6
Rohit Pandey <croods660@gmail.com>
2024-05-19 13:24:12 +0530

    some things added

--- enter_pin.py
+++ enter_pin.py
@@ -1,10 +1,21 @@
 
-pin = 2345
+# pin = 2345
 
-while True:
-    user_input = int(input("Enter your 4 digit pin: "))
-    if user_input == pin:
-         print("Pin accepted")
-         break
+# while True:
+#     user_input = int(input("Enter your 4 digit pin: "))
+#     if user_input == pin:
+#          print("Pin accepted")
+#          break
+#     else:
+#         print("Invalid pin. Please try again.")
+
+
+number = 0
+
+while number < 10:
+    print(number)
+    number += 1
+    if number == 11:
+        break
     else:
-        print("Invalid pin. Please try again.")
+        print("We are done.")
