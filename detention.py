462aabec99468b3c6fe795022f7fcc17975d8660
Rohit Pandey <croods660@gmail.com>
2024-05-19 13:37:08 +0530

    one more basic program

---
+++ detention.py
@@ -0,0 +1,8 @@
+
+# for i in range(1, 101):
+#     print(f"{i} I will not use Snapchat in class.")
+
+for i in range(99, 0, -1):
+   print(f"{i} bottles of beer on the wall, {i} bottles of beer. Take one down, pass it around, {i-1} bottles of beer on the wall.")
+
+
