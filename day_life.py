432d6652c579ace17c21f2314fe5d67549b809f5
Rohit Pandey <croods660@gmail.com>
2024-05-19 19:58:05 +0530

    Day 17 of 50: Today's Achievement: Building a Terminal-Based Choice Game

---
+++ day_life.py
@@ -0,0 +1,160 @@
+
+
+print("Welcome to the most interesting game of your life!🙂🙂")
+print("You are about to embark on a journey that will change your life forever.🌟🌟")
+print("You will be faced with choices that will determine your destiny.🔮🔮")
+
+my_choice = input("Are you ready to begin? (yes/no) ")
+
+if my_choice.lower() == "no":
+    print("Goodbye!👋👋")
+    exit()
+else:
+    print("Great! Let's get started.👍👍")
+
+my_name = input("What is your name? ")
+print(f"Hello, {my_name}!👋👋")
+
+print("You are doing poopoo in the toilet and you realized something is wrong?")
+
+print("What do you do?")
+
+print("1) Call for help") #Please choose this option
+# print("2) Try to fix it yourself")
+# print("3) Ignore it and walk away")
+
+choice_1 = int(input("Enter your choice: "))
+
+if choice_1 == 1:
+    print("You called for help and your gf came to help you. She found nothing wrong and laughed at you.🤣🤣")
+    print("You are embarrassed but you are happy that she is there for you.🥰🥰")
+    print("You again started doing poopoo and you realized there is something wrong again.🤔🤔")
+    print("This time you decided to fix it yourself.🔧🔧")
+    print("You just moved your head and saw there is a weird looking creature in the toilet.👽👽")
+    print("What do you do?")
+    print("1) Scream and run away")
+    print("2) Try to kill it")
+    print("3) Try to talk to it")
+
+    choice_2 = int(input("Enter your choice: "))
+
+    if choice_2 == 1:
+        print("You screamed and ran away.🏃‍♂️🏃‍♂️")
+        print("You told your gf about it and she laughed at you again.🤣🤣")
+        print("But You are safe because creature didn't followed you.👍👍")
+
+    elif choice_2 == 2:
+        print("You tried to kill it But he was so strong that he killed you instead.💀💀")
+        print("Game Over!😭😭")
+        exit()
+
+
+    elif choice_2 == 3:
+        print("You tried to talk to it.🗣🗣")
+        print("It turns out that he is a friendly creature and he just wanted your help.🤝🤝")
+        print("You Said what help do you need?🤔🤔")
+        print("He said a very big creature is eating our village people's and we need your help to save them.👨‍👩‍👧‍👦👨‍👩‍👧‍👦")
+        print("What do you do?")
+        print("1) Agree to help")
+        print("2) Refuse to help")
+
+        choice_3 = int(input("Enter your choice: "))
+
+        if choice_3 == 1:
+            print("You agreed to help.👍👍")
+            print("Creature opened a portal and you both went to his village.🌌🌌")
+            print("You saw his village and it's so beautiful.🌈🌈")
+            print("You are amazed by the beauty of the village.😍😍")
+            print("You asked him But how i will help you?🤔🤔")
+            print("I am just a normal human.🤷‍♂️🤷‍♂️")
+            print("I work as a software developer i don't have any superpowers.🏢🏢")
+            print("He said you are not a normal human you are the chosen one.👑👑")
+            print("You have the power to save us.🦸‍♂️🦸‍♂️")
+            print("He tells you that you have so many powers that you don't know about.🔮🔮")
+            print("Especially, you have the power to control the elements.🌊🔥🌪️🌬️")
+            print("You are shocked and amazed at the same time.😳😳")
+            print("You are ready to save the village people.👊👊")
+            print("He gave you a old mansion key and said you stay here and rest.🏚️🏚️")
+            print("You went to the mansion and tried to sleep but you can't sleep.😴😴")
+            print("You are thinking about the village people and how you can save them.🤔🤔")
+            print("You decided to roam around the village and see if you can find something.🚶‍♂️🚶‍♂️")
+            print("You saw a gorgeous lady stucked in a hole.👩‍🦰👩‍🦰")
+            print("What do you do?")
+            print("1) Help her")
+            print("2) Ignore her")
+
+            choice_4 = int(input("Enter your choice: "))
+
+            if choice_4 == 1:
+                print("You helped her and she said thank you.🙏🙏")
+                print("You asked her why you are here?🤔🤔")
+                print("This is a dangerous place.🚫🚫")
+                print("You have to be in your house and stay safe.🏠🏠")
+                print("You have to sleep this time not roam around the village.😴😴")
+                print("She said actually i live in a orphanage and i was lost.🏚️🏚️")
+                print("I don't know how i came here.🤷‍♀️🤷‍♀️")
+                print("You said don't worry i will take you to the orphanage.👍👍")
+                print("She said, No Thanks you already helped me.🙏🙏")
+                print("You said, No i can't leave you alone in this dangerous place.🚫🚫")
+                print("She said, I am not a child i can take care of myself.👩👩")
+                print("You said, Okey, But be careful.👍👍")
+                print("You said her you remember the way to the orphanage?🤔🤔")
+                print("She said, No But i will find.👍👍")
+                print("You said, Okey, Take care.👋👋")
+                print("She started walking and you secretly followed her.🚶‍♂️🚶‍♂️")
+                print("And you saw some dangerous creatures are started attacking her.👹👹")
+                print("What do you do?")
+                print("1) Save her")
+                print("2) Run away")
+
+                choice_5 = int(input("Enter your choice: "))
+
+                if choice_5 == 1:
+                    print("You saved her and she said thank you.🙏🙏")
+                    print("You said, I told you to be careful.🚫🚫")
+                    print("She said, I am sorry i was lost.🤷‍♀️🤷‍♀️")
+                    print("You said, it's so late, why not you stay with me tonight?🌃🌃")
+                    print("She said, No, I can't.🚫🚫")
+                    print("You said, But it's not safe to roam in the night.🌌🌌")
+                    print("She said, I will be fine.👍👍")
+                    print("You said, Just come with me i can't leave you alone.👫👫")
+                    print("She said, Okey, But only for tonight.🌃🌃")
+                    print("You both went to the mansion and you gave her a room.🏠🏠")
+                    print("You said, Good night.😴😴")
+                    print("She said, Good night.😴😴")
+                    print("You both slept.😴😴")
+                    print("Next morning, you woke up and she was gone.🌅🌅")
+                    print("You searched for her but she was nowhere.🔍🔍")
+                    print("You went to the creature and asked him when i have to kill the big creature?🤔🤔")
+                    print("He said, You have to kill him tonight.🌃🌃")
+                    print("You said, But how?🤔🤔")
+                    print("He said, You have to use your powers.🔮🔮")
+                    print("You said, I don't know how to use them.🤷‍♂️🤷‍♂️")
+                    print("He said, You have to believe in yourself.👊👊")
+                    print("You said, Okey, I will try.👍👍")
+                    print("You went to the big creature's cave in night.🦖🦖")
+                    print("You saw him and you started using your powers.🔮🔮")
+                    print("You are not able to control them.🚫🚫")
+                    print("What's happening with me you said?🤔🤔")
+                    print("Then, You closed Your Eyes.")
+                    print("And You heard a voice, Believe in yourself.🔮🔮")
+                    print("You opened your eyes and you started using your powers.🔮🔮")
+                    print("You killed the big creature.🦖🦖")
+
+                if choice_5 == 2:
+                    print("You ran away and she died.💀💀")
+                    print("Game Over!😭😭")
+                    exit()
+
+        if choice_3 == 2:
+                print("You refused to help.🚫🚫")
+                print("Creature got angry and killed you.💀💀")
+                print("Game Over!😭😭")
+                exit()
+
+
+if choice_1 == 2:
+    pass
+
+if choice_1 == 3:
+    pass
