27abd7d2b0deff29ac0cf37ba4f96811d20e0fa0
Rohit Pandey <croods660@gmail.com>
2024-05-20 16:09:20 +0530

    Revision of python basics part 1 completed

---
+++ revision.py
@@ -0,0 +1,82 @@
+
+# print("Hello, World!")
+# print(True)
+# print(2 + 2)
+# print(1000)
+
+# name = input("Enter your name: ")
+# print(name)
+
+# # print("i am a comment")
+
+# grade = int(input("Enter your marks: "))
+
+# if grade >= 90:
+#     print("Grade A")
+
+# elif grade >= 80:
+#     print("Grade B")
+
+# elif grade >= 70:
+#     print("Grade C")
+
+# else:
+#     print("Grade D")
+
+fruit = "apple"
+
+gravity = 9.8
+
+print(fruit + " " + str(gravity))
+
+number = 7777777
+pi = 3.14
+greetings = "Hello, World!"
+earth_is_flat = False
+
+print(str(number) + " " + str(pi) + " " + str(greetings) + " " + str(earth_is_flat))
+
+a = 10
+b = 20
+
+# if a and b == 20:
+#     print("Both are 30")
+# else:
+#     print("Both are not 30")
+
+# if a or b == 20:
+#     print("One is 20")
+# else:
+#     print("Both are not 20")
+
+# if not a == 20:
+#     print("a is not 20")
+# else:
+#     print("b is 20")
+
+# while a < 20:
+#     print(a)
+#     a += 1
+    
+
+# for i in range(1, 101):
+#     print(f"{i}: Welcome to Python!")
+
+# import random
+
+# num = random.randint(1, 100)
+# print(num)
+
+addition = 10 + 50
+
+subtraction = 50 - 10
+
+multiplication = 10 * 50
+
+divide = 50 / 10
+
+modulus = 50 % 10
+
+exponents = 10 ** 2
+
+print(addition, subtraction, multiplication, divide, modulus, exponents)
